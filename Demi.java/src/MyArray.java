af9991043922ac570977d32316f8cd7803a67f3e
Ranveer Mali <ranveermali4142@mail.com>
2026-07-27 12:10:34 +0530

    New Activity Added

---
+++ Demi.java/src/MyArray.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class MyArray {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int[] arr=new int[5];
+        arr[0]=10;
+
+        for(int item :arr){
+            System.out.println(item);
+
+            
+        }
+    }
+}
